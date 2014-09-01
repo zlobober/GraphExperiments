a9324db9f87a3c5a2fbb472c2f8aec463bbc8320
Max Akhmedov <lafaam@gmail.com>
2014-09-01 03:04:36 -0700

    fixed table.py

--- gen/table.py
+++ gen/table.py
@@ -1,7 +1,7 @@
 from sys import argv
 from json import dumps
 a, b = map(int, argv[1:])
-nodes = {i : [i % b, i // b] for i in range(a * b)}
+nodes = [[i % b, i // b] for i in range(a * b)]
 edges = [[i, i + b] for i in range(a * b - b)] + [[i, i + 1] for i in range(a * b) if i % b != b - 1]
 D = {'nodes' : nodes, 'edges' : edges}
 print(dumps(D))
