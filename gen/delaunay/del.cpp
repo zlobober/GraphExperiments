bb515e8fcd35584c5c5599f5765c87ea9a9a0faf
Max Akhmedov <lafaam@gmail.com>
2014-09-01 15:52:44 -0700

    added crossings

--- gen/delaunay/del.cpp
+++ gen/delaunay/del.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
+#include <set>
 #include <cassert>
 using namespace std;
 
@@ -53,14 +54,48 @@ struct vt
     vt() {}
 };
 
+set<pair<int, int> > crossed;
+
+const int N = 2000;
+vector<int> E[N];
+
+int tmp[N];
+
+vector<pair<int, int> > ed;
+bool try_cross(int a, int b)
+{
+    int q = set_intersection(E[a].begin(), E[a].end(), E[b].begin(), E[b].end(), tmp) - tmp;
+    if (q != 2)
+        return false;
+    int c = tmp[0], d = tmp[1];
+    int U[] = {
+        a, b,
+        a, c,
+        a, d,
+        b, c,
+        b, d,
+        c, d,
+    };
+    for (int u = 0; u < 5; u++)
+        if (crossed.find(make_pair(U[2 * u], U[2 * u + 1])) != crossed.end())
+            return false;
+    E[c].push_back(d);
+    E[d].push_back(c);
+    for (int u = 0; u < 6; u++)
+        crossed.insert(make_pair(U[2 * u], U[2 * u + 1])), crossed.insert(make_pair(U[2 * u + 1], U[2 * u]));
+    ed.push_back(make_pair(c, d));
+    return true;
+}
+
 int main(int argc, char* argv[])
 {
-    assert(argc == 6);
+    assert(argc == 7);
     int m = atoi(argv[1]);
     int k = atoi(argv[2]);
-    FILE* pts = fopen(argv[3], "r");
-    FILE* tri = fopen(argv[4], "r");
-    srand(atoi(argv[5]));
+    int c = atoi(argv[3]);
+    FILE* pts = fopen(argv[4], "r");
+    FILE* tri = fopen(argv[5], "r");
+    srand(atoi(argv[6]));
     int n;
     assert(fscanf(pts, "%d", &n));
     vector<int> bombs(n, 0);
@@ -74,35 +109,34 @@ int main(int argc, char* argv[])
     for (int i = 0; i < n; i++)
         printf("\t\t[%d, %d, %d]%c\n", P[i].x, P[i].y, bombs[i], ", "[i + 1 == n]);
     printf("],\n\t\"edges\": [\n");
-    vector<pair<int, int> > E;
     while (true)
     {
         int a, b, c;
         if (fscanf(tri, "%d %d %d", &a, &b, &c) == 3)
         {
             --a, --b, --c;
-            E.push_back(make_pair(a, b));
-            E.push_back(make_pair(b, c));
-            E.push_back(make_pair(c, a));
+            ed.push_back(make_pair(a, b));
+            ed.push_back(make_pair(b, c));
+            ed.push_back(make_pair(c, a));
         }
         else
             break;
     }
-    sort(E.begin(), E.end());
-    E.resize(unique(E.begin(), E.end()) - E.begin());
-    random_shuffle(E.begin(), E.end());
-    E.resize(m);
-    for (int i = 0; i < E.size(); i++)
+    sort(ed.begin(), ed.end());
+    ed.resize(unique(ed.begin(), ed.end()) - ed.begin());
+    random_shuffle(ed.begin(), ed.end());
+    ed.resize(m);
+    for (int i = 0; i < ed.size(); i++)
     {
         here:;
-        if (i == E.size())
+        if (i == ed.size())
             break;
-        for (int j = 0; j < E.size(); j++)
+        for (int j = 0; j < ed.size(); j++)
         {
             if (i == j)
                 continue;
-            int a[2] = {E[i].first, E[i].second};
-            int b[2] = {E[j].first, E[j].second};
+            int a[2] = {ed[i].first, ed[i].second};
+            int b[2] = {ed[j].first, ed[j].second};
             int c = -1;
             for (int u = 0; u < 2; u++)
                 for (int v = 0; v < 2; v++)
@@ -118,14 +152,33 @@ int main(int argc, char* argv[])
                 vt vb = P[b[1]] - P[b[0]];
                 if (abs(atan2(va ^ vb, va * vb)) < 3.1415926 / 10)
                 {
-                    E.erase(E.begin() + ((rand() & 1) ? i : j));
+                    ed.erase(ed.begin() + ((rand() & 1) ? i : j));
                     goto here;
                 }
             }
         }
     }
-    for (int i = 0; i < E.size(); i++)
-        printf("\t\t[%d, %d]%c\n", E[i].first, E[i].second, ", "[i + 1 == E.size()]);
+
+    for (int i = 0; i < ed.size(); i++)
+    {
+        E[ed[i].first].push_back(ed[i].second);
+        E[ed[i].second].push_back(ed[i].first);
+    }
+
+    for (int i = 0; i < c; i++)
+    {
+        while (true)
+        {
+            int q = rand() % ed.size();
+            if (try_cross(ed[q].first, ed[q].second))
+                break;
+        }
+    }
+
+    random_shuffle(ed.begin(), ed.end()); 
+
+    for (int i = 0; i < ed.size(); i++)
+        printf("\t\t[%d, %d]%c\n", ed[i].first, ed[i].second, ", "[i + 1 == ed.size()]);
     printf("\t]\n}\n");
 
 }
--- gen/delaunay/gen.sh
+++ gen/delaunay/gen.sh
@@ -11,13 +11,13 @@ if [ ! -f ./del ]; then
 fi
 
 if (( $# < 5 )); then
-    echo "./gen.sh <number of nodes> <number of bombs> <number of edges> <maximal coordinates> <seed>"
+    echo "./gen.sh <number of nodes> <number of bombs> <number of edges> <maximal coordinates> <number of crossings> <seed>"
     exit 1
 fi
 
-timeout 5.0 ./gen_rand $1 $4 $5 > del_nodes.txt
+timeout 5.0 ./gen_rand $1 $4 $6 > del_nodes.txt
 tail -n $1 del_nodes.txt > del2_nodes.txt
 timeout 5.0 ./delaunay del2 > /dev/null
-timeout 5.0 ./del $3 $2 del_nodes.txt del2_elements.txt $5
+timeout 5.0 ./del $3 $2 $5 del_nodes.txt del2_elements.txt $6
 
 popd >/dev/null
