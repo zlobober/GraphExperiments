35dc43b462a2f973f1a336bcfd536e6f09695669
Max Akhmedov <lafaam@gmail.com>
2014-09-01 06:55:20 -0700

    fixed .py

--- gen/delaunay.py
+++ gen/delaunay.py
@@ -1,4 +1,6 @@
 import cgitb
+import sys
+from time import time
 cgitb.enable()
 
 print("Content-Type: application/json")
@@ -6,6 +8,39 @@ print()
 
 import cgi
 form = cgi.FieldStorage()
-if not "nodes" in form:
-    print("Expected nodes GET parameter")
 
+bad = False
+
+result = dict()
+error = ''
+
+if not "nodes" in form:
+    error += "Expected nodes GET parameter\n"
+else:
+    nodes = int(form["nodes"].value)
+
+if not "bombs" in form:
+    error += "Expected bombs GET parameter\n";
+else:
+    bombs = int(form["nodes"].value)
+
+if not "edges" in form:
+    error += "Expected edges GET parameter\n";
+else:
+    edges = int(form["nodes"].value)
+
+if not "maxc" in form:
+    error += "Expected maxc GET parameter\n";
+else:
+    maxc = int(form["nodes"].value)
+
+if not "seed" in form:
+    seed = int(time())
+else:
+    seed = int(form["seed"].value)
+
+bashCommand = "/var/www/akhmedov.org/bomboleo/gen/delaunay/gen.sh %d %d %d %d %d" % (nodes, bombs, edges, maxc, seed)
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print(output)
